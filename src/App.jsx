6eef62fdac087966e0d6d8a4a0000f5a1812e282
Nikhil <jhanikhil881@gmail.com>
2023-12-03 20:24:57 +0530

    Add existing project files to Git

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import  Nav  from "./Components/Nav";
+import {SuperQuality,Subscribe,SpecialOffers,Services,PopularProduct,Hero,Footer,CustomerReviews} from './index'
+
+const App = () => {
+  return (
+    <main className='relative'>
+      <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
+      <section className='padding'>
+        <PopularProduct />
+      </section>
+      <section className='padding'>
+        <SuperQuality />
+      </section>
+      <section className='padding-x py-10'>
+        <Services />
+      </section>
+      <section className='padding'>
+        <SpecialOffers />
+      </section>
+      <section className='bg-pale-blue padding'>
+        <CustomerReviews />
+      </section>
+      <section className='padding-x sm:py-32 py-16 w-full'>
+        <Subscribe />
+      </section>
+      <section className=' bg-black padding-x padding-t pb-8'>
+        <Footer />
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,23 @@
+import CustomerReviews from "./section/CustomerReviews";
+import Footer from "./section/Footer";
+import Hero from "./section/Hero";
+import PopularProduct from "./section/PopularProduct";
+import Services from "./section/Services";
+import SpecialOffers from "./section/SpecialOffers";
+import Subscribe from "./section/Subscribe";
+import SuperQuality from "./section/SuperQuality";
+import Nav from "./section/nav";
+
+
+export {
+    CustomerReviews,
+    Footer,
+    Hero,
+    PopularProduct,
+    Services,
+    SpecialOffers,
+    Subscribe,
+    SuperQuality,
+    Nav
+
+}
